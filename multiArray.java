08112fb6fefe24a0a720f028edb3b6ec233319e4
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2022-10-13 13:02:15 +0530

    Multi-Dimensional Arrays

---
+++ multiArray.java
@@ -0,0 +1,15 @@
+public class multiArray {
+  public static void main(String[] args) {
+    int[][] numbers = { {4, 2, 1, 7},
+    					  {8, 2, 5}
+                        };
+    int x = numbers [0][0];
+    int y = numbers [0][1];
+    int z = numbers [0][2];
+    int m = numbers [0][3];
+    int n = numbers [1][0];
+    int p = numbers [1][1];
+    int q = numbers [1][2];
+    System.out.println(x+" "+y+" "+z+" "+m+" "+n+" "+p+" "+q);
+  }
+}
